ea69d1a443ac8e5226d8a27b74066bc4c9a70c18
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-08-17 10:05:28 -0600

    text editor and button

--- minij/minij/Form1.cs
+++ minij/minij/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,40 @@ namespace minij
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFD = new OpenFileDialog();
+            oFD.Filter = "txt files (*.txt)|*.txt";
+            if (oFD.ShowDialog() == DialogResult.OK)
+            {
+                fileTextBox.Text = Read(oFD.FileName);
+            }
+        }
+
+        public string Read(string path)
+        {
+            string freaded = "";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                freaded = sr.ReadToEnd();
+            }
+            return freaded;
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFD = new SaveFileDialog();
+            saveFD.Filter = "txt files (*.txt)|*.txt";
+            if (saveFD.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFD.OpenFile()))
+                {
+                    writer.WriteLine(fileTextBox.Text);
+                    writer.Dispose();
+                }
+            }
         }
     }
 }
