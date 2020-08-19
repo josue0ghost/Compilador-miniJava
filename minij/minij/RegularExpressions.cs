d125f107fbcbb8751e0054fd885a8d63fbca100b
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-08-18 22:21:06 -0600

    Create RegularExpressions.cs
    Clase para manejar las expresiones regulares

---
+++ minij/minij/RegularExpressions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace minij
+{
+	static class RegularExpressions
+	{
+		// Formato:
+		//static Regex id = new Regex("pattern");
+
+	}
+}
