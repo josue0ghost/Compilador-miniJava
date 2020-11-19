b95db7398f4511d18f9ac0938129ce5c20df8ac7
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-11-19 11:26:08 -0600

    Tabla

---
+++ minij/minij/TDSobj.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minij
+{
+    class TDSobj
+    {
+        /* type
+         * 0 = null
+         * 1 = int
+         * 2 = double
+         * 3 = boolean
+         * 4 = string
+         * 5 = void
+         */
+        public int type { get; set; }
+
+        /*_base
+         * 0 = NaN
+         * 1 = decimal
+         * 2 = octal
+         * 3 = hex
+         */
+        public int _base { get; set; }
+
+        /* value
+         * string con el valor
+         */
+        public string value { get; set; }
+
+        public string name { get; set; }
+
+        /* key = idAmbito,name */
+        public int idAmbito { get; set; }
+
+        public TDSobj(int iType, int Base, string Name, string Value, int iAmbito)
+        {
+            this.type = iType;
+            this._base = Base;
+            this.value = Value;
+            this.name = Name;
+            this.idAmbito = iAmbito;
+        }
+
+        public TDSobj(int iType, string Name, string Value, int iAmbito)
+        {
+            this.type = iType;
+            this.value = Value;
+            this.name = Name;
+            this.idAmbito = iAmbito;
+            this._base = 0;
+        }
+    }
+}
---
+++ minij/minij/Tabla de símbolos.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minij
+{
+    class Tabla_de_símbolos
+    {
+        /* table struct
+         * idAmbito, name | type | value | _base
+         */
+
+        public Dictionary<string, string> table = new Dictionary<string, string>();
+
+        public void Insert(TDSobj symbol)
+        {
+            string key = symbol.idAmbito.ToString() + "," + symbol.name;
+            string value = symbol.type.ToString() + "," + symbol.value + "," + symbol._base;
+
+            if (!table.ContainsKey(key))
+            {
+                table.Add(key, value);
+            }
+            else
+            {
+                // error
+            }
+        }
+
+        public void Delete(TDSobj symbol)
+        {
+            string key = symbol.idAmbito.ToString() + "," + symbol.name;
+
+            if (table.ContainsKey(key))
+            {
+                table.Remove(key);
+            }
+            else
+            {
+                // error
+            }
+        }
+
+        public void Delete(int idAmbito, string name)
+        {
+            string key = idAmbito.ToString() + "," + name;
+
+            if (table.ContainsKey(key))
+            {
+                table.Remove(key);
+            }
+            else
+            {
+                // error
+            }
+        }
+
+        public TDSobj Search(TDSobj symbol)
+        {
+            string key = symbol.idAmbito.ToString() + "," + symbol.name;
+
+            if (table.ContainsKey(key))
+            {
+                table.TryGetValue(key, out string value);
+                string[] vals = value.Split(',');
+                TDSobj obj = new TDSobj(int.Parse(vals[0]), vals[1], vals[2], int.Parse(vals[3]));
+                return obj;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public TDSobj Search(int idAmbito, string name)
+        {
+            string key = idAmbito.ToString() + "," + name;
+
+            if (table.ContainsKey(key))
+            {
+                table.TryGetValue(key, out string value);
+                string[] vals = value.Split(',');
+                TDSobj obj = new TDSobj(int.Parse(vals[0]), vals[1], vals[2], int.Parse(vals[3]));
+                return obj;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
