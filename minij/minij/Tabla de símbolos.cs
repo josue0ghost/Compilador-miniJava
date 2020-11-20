59987c68b938b62a92c5e2dc6d165d83c923c323
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-11-20 01:24:46 -0600

    compare types

--- minij/minij/Tabla de símbolos.cs
+++ minij/minij/Tabla de símbolos.cs
@@ -90,5 +90,73 @@ namespace minij
                 return null;
             }
         }
+        
+        public bool compareTypes(int idAmbito, string varName1, string varName2)
+        {
+            string key1 = idAmbito.ToString() + "," + varName1;
+            string key2 = idAmbito.ToString() + "," + varName2;
+
+            if (table.TryGetValue(key1, out string val1))
+            {
+                if (table.TryGetValue(key2, out string val2))
+                {
+                    string[] data1 = val1.Split(',');
+                    string[] data2 = val2.Split(',');
+
+                    if (data1[0] == data2[0])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    //error, no existe la variable 2
+                    return false;
+                }
+            }
+            else
+            {
+                // error, no existe la variable 1
+                return false;
+            }
+        }
+
+        public bool compareTypes(int idAmbito, string varName1, string valueOf2, int typeOfValue)
+        {
+            string key1 = idAmbito.ToString() + "," + varName1;
+
+            if (table.TryGetValue(key1, out string val1))
+            {
+                string[] data1 = val1.Split(',');
+
+                if (data1[0] == typeOfValue.ToString()) // mismos tipos
+                {
+                    return true;
+                }
+                else if (data1[0] == "2" && typeOfValue == 1) // asignacion de entero (type 1) a un double (type 2)
+                {
+                    return true;
+                    /*
+                    data1[1] = valueOf2 + ".0";
+                    string newVal = data1[0] + "," + data1[1] + "," + data1[2];
+                    // cambio de atributos
+                    table[key1] = newVal;
+                    */
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                // error, no existe la variable 1
+                return false;
+            }
+        }
     }
 }
